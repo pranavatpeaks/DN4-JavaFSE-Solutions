184009ac54dd5370370391c582226a2deeb42501
pranavatpeaks <118458502+pranavatpeaks@users.noreply.github.com>
2025-06-22 00:04:27 +0530

    Delete Week 1 - Data Structures and Algorithms/FinancialForecasting directory

--- Week 1 - Data Structures and Algorithms/FinancialForecasting/src/Forecaster.java
+++
@@ -1,9 +0,0 @@
-public class Forecaster {
-    private final ForecastingStrategy strategy;
-    public Forecaster(ForecastingStrategy strategy) {
-        this.strategy = strategy;
-    }
-    public double forecast(double principal, double rate, int timePeriod) {
-        return strategy.calculateForecast(principal, rate, timePeriod);
-    }
-}
--- Week 1 - Data Structures and Algorithms/FinancialForecasting/src/ForecastingStrategy.java
+++
@@ -1,3 +0,0 @@
-public interface ForecastingStrategy {
-    double calculateForecast(double principal, double rate, int timePeriod);
-}
